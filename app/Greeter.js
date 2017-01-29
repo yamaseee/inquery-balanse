52a462dea4780d368a86828398029d936c930679
yamaseee <yamaseee@gmail.com>
2017-01-29 20:16:53 +0700

    all css until HtmlWebpackPlugin

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react'
 import config from './config.json';
+import styles from './Greeter.css';
 
 class Greeter extends Component{
   render() {
-    return (<div>
+    return (<div className={styles.root}>
               {config.greetText}
             </div>
           );
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack');
+
 module.exports = {
   devtool: 'eval-source-map',
   entry: __dirname + "/app/main.js",
@@ -15,9 +17,22 @@ module.exports = {
       test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel'
+    },
+    {
+      test: /\.css$/,
+      loader: 'style!css?modules!postcss'
     }
   ]
   },
+
+  postcss: [
+    require('autoprefixer')
+  ],
+
+  plugins: [
+    new webpack.BannerPlugin("Copyright Flying Unicorns inc.")
+  ],
+
   devServer: {
     contentBase: "./public",
     colors: true,
